bfa1c9081a96e96ac39600ba33833a8eaec7beeb
lorenzosilicato <lollo.silicato@gmail.com>
2024-04-03 09:49:44 +0200

    aggiustamenti esercizi

--- src/routes/esercizi/data.js
+++ src/routes/esercizi/data.js
@@ -3,19 +3,125 @@ export const posts = [
 		slug: 'exercise1',
 		title: 'Exercise 1',
 		content:
-      'https://i.simmer.io/@lorenzosilicato/esercizio1'
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse1_12'
   },
 
 	{
 		slug: 'exercise2',
 		title: 'Exercise 2',
 		content:
-      '<p>Qui andrebbe inserito il file WebGL dell esercizio</p>'
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse2'
 	},
 	{
 		slug: 'exercise3',
 		title: 'Exercise 3',
 		content:
-      '<p>Qui andrebbe inserito il file WebGL dell esercizio</p>'
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse3'
+  },
+  {
+		slug: 'exercise4',
+		title: 'Exercise 4',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse4_15'
+  },
+
+	{
+		slug: 'exercise5',
+		title: 'Exercise 5',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse2'
+	},
+	{
+		slug: 'exercise6',
+		title: 'Exercise 6',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse6_9_19'
+  },
+	{
+		slug: 'exercise7',
+		title: 'Exercise 7',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse7_18'
+  },
+
+	{
+		slug: 'exercise8',
+		title: 'Exercise 8',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse8_13'
+	},
+	{
+		slug: 'exercise9',
+		title: 'Exercise 9',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse6_9_19'
+  },
+  {
+		slug: 'exercise10',
+		title: 'Exercise 10',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse10'
+  },
+	{
+		slug: 'exercise11',
+		title: 'Exercise 11',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse11'
+  },
+
+	{
+		slug: 'exercise12',
+		title: 'Exercise 12',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse1_12'
+	},
+	{
+		slug: 'exercise13',
+		title: 'Exercise 13',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse8_13'
+  },
+  {
+		slug: 'exercise14',
+		title: 'Exercise 14',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse20'
+  },
+	{
+		slug: 'exercise15',
+		title: 'Exercise 15',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse4_15'
+  },
+
+	{
+		slug: 'exercise16',
+		title: 'Exercise 16',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse17'
+	},
+	{
+		slug: 'exercise17',
+		title: 'Exercise 17',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse17'
+  },
+  {
+		slug: 'exercise18',
+		title: 'Exercise 18',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse7_18'
+  },
+	{
+		slug: 'exercise19',
+		title: 'Exercise 19',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse6_9_19'
+	},
+	{
+		slug: 'exercise20',
+		title: 'Exercise 20',
+		content:
+      'https://student3.iccsa.org/EserciziCompilati/BuildEse20'
   }
 ];
